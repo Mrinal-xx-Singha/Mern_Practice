09a67f520c9b83b3baae31b54ef571900f43fc69
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-02-20 20:12:00 +0530

    creating a ecommerce store using redis

---
+++ store/backend/controllers/authController.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup called");
+};
+
+export const login = async (req, res) => {
+  res.send("Login called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout called");
+};
---
+++ store/backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./utils/db.js";
+const app = express();
+dotenv.config();
+
+app.use("/api/auth", authRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} ✅`);
+  connectDB();
+});
