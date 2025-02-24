e15af604776cd0276a44f783d9dd451bc09afdd5
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-02-24 11:57:02 +0530

    created feature products route to get all featured  products

--- store/backend/controllers/productController.js
+++ store/backend/controllers/productController.js
@@ -1,4 +1,7 @@
 import Product from "../models/productModel.js";
+import { redis } from "../utils/redis.js";
+
+
 export const getAllProducts = async (req, res) => {
   try {
     // Empty filter find all products
@@ -10,12 +13,37 @@ export const getAllProducts = async (req, res) => {
 };
 
 
-export const getFeaturedProducts  = async(req,res) =>{
-    try {
-        
-        
-    } catch (error) {
-        
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+    // if it is not in redis, fetch from mongodb
+    // .lean() returns a js object insted of mongodb document which is good for performance
+
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
     }
 
+    // Store in redis for future quick access
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.status(200).json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const createProduct = async(req,res) =>{
+  try {
+    
+  } catch (error) {
+    
+  }
 }
--- store/backend/routes/productRoutes.js
+++ store/backend/routes/productRoutes.js
@@ -1,13 +1,14 @@
-import express from "express"
-import { getAllProducts } from "../controllers/productController.js"
-import { protectRoute } from "../middleware/authMiddleware.js"
-import { adminRoute } from "../middleware/authMiddleware.js"
-import { getFeaturedProducts } from "../controllers/productController.js"
-
-const router = express.Router()
-
-router.get("/",protectRoute,adminRoute,getAllProducts)
-router.get("/",protectRoute,adminRoute,getFeaturedProducts)
+import express from "express";
+import { createProduct, getAllProducts } from "../controllers/productController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { adminRoute } from "../middleware/authMiddleware.js";
+import { getFeaturedProducts } from "../controllers/productController.js";
 
 
-export default router
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/product", protectRoute, adminRoute, createProduct);
+
+export default router;
