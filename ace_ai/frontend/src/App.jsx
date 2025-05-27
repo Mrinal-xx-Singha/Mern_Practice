f5af51c3faebd0ea874790dc3617d2ffd60dc224
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-05-27 14:39:25 +0530

    route structure

--- ace_ai/frontend/src/App.jsx
+++ ace_ai/frontend/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 
 const App = () => {
   return (
-    <div className=''>
-      App
+    <div className="">
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </Router>
+      <Toaster
+        toastOptions={{
+          className: "",
+          styles: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
